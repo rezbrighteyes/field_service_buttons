1cefa3c4d4d68dc0c1e2762762b935a9c1f2fe5d
Amirreza Delavaran Shiraz <rez@Amirrezas-MacBook-Pro.local>
2026-05-18 15:12:59 +1000

    fix: stop loading Liaise worksheet view without model

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,6 @@
         'security/ir.model.access.csv',
         'views/project_task_views.xml',
         'views/assets.xml',
-        'views/liaise_worksheet_report.xml',
     ],
     'assets': {
         'web.assets_backend': [
